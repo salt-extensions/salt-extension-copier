035d31cb219566926ecaa4f6833d2ed6d13861f3
jeanluc <lkubb@protonmail.com>
2026-07-02 19:43:07 +0200

    Minor adjustments
    * ensure rendered files pass pre-commit in the first run
    * add new dunders to ruff convenience
    * pylint ignore missing setuptools

--- project/setup.py
+++ project/setup.py
@@ -1,5 +1,5 @@
 # pylint: disable=missing-module-docstring
-import setuptools
+import setuptools  # pylint: disable=import-error
 
 if __name__ == "__main__":
     setuptools.setup(use_scm_version=True)
